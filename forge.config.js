92a1072201dd653a2d17857203c00c3be022e6d5
ThePowerUsers <ifuizu1@gmail.com>
2026-09-21 13:12:51 +0600

    Reduce Windows package locales

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    electronLanguages: ['en-US', 'en'],
   },
   rebuildConfig: {},
   makers: [
